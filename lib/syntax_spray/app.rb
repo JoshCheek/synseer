0053029aa25c3ea91bfbe801f0294f87e181814f
Josh Cheek <josh.cheek@gmail.com>
2015-11-04 00:56:10 -0700

    App has a real "game" in it

--- lib/syntax_spray/app.rb
+++ lib/syntax_spray/app.rb
@@ -1,18 +1,40 @@
+require 'pathname'
+require 'erb'
+
 module SyntaxSpray
   class App
+
     def self.default
-      new
+      return @default if defined? @default
+      root_dir    = Pathname.new File.expand_path('../..', __dir__)
+      games_dir   = root_dir / 'games'
+      named_games = games_dir.children.map do |child|
+        [child.basename.sub_ext('').to_s.intern, child.read]
+      end
+      @default    = new named_games.to_h
+    end
+
+    attr_accessor :games
+
+    def initialize(games)
+      self.games = games
     end
 
     def call(env)
-      body = <<-BODY
+      template = <<-BODY
         <!DOCTYPE html>
         <html>
         <body>
-        <div class="available_games">game</div>
+        <div class="available_games">
+          <% games.each do |name, code| %>
+            <%= name %>
+          <% end %>
+        </div>
         </body>
         </html>
       BODY
+
+      body = ERB.new(template).result(binding)
       [200,{'Content-Type' => 'text/html', 'Content-Length' => body.length.to_s},[body]]
     end
   end
