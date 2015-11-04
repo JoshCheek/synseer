f878849e4bed73dcad7834fe1b094629c1681c67
Josh Cheek <josh.cheek@gmail.com>
2015-11-04 01:00:23 -0700

    Add a Game class

--- lib/syntax_spray/app.rb
+++ lib/syntax_spray/app.rb
@@ -2,16 +2,24 @@ require 'pathname'
 require 'erb'
 
 module SyntaxSpray
+  class Game
+    attr_accessor :name, :code
+    def initialize(name, code)
+      self.name, self.code = name, code
+    end
+  end
+
   class App
 
     def self.default
       return @default if defined? @default
-      root_dir    = Pathname.new File.expand_path('../..', __dir__)
-      games_dir   = root_dir / 'games'
-      named_games = games_dir.children.map do |child|
-        [child.basename.sub_ext('').to_s.intern, child.read]
+      root_dir  = Pathname.new File.expand_path('../..', __dir__)
+      games_dir = root_dir / 'games'
+      games     = games_dir.children.map do |child|
+        name = child.basename.sub_ext('').to_s.gsub("_" , " ")
+        Game.new name, child.read
       end
-      @default    = new named_games.to_h
+      @default = new games
     end
 
     attr_accessor :games
@@ -26,8 +34,8 @@ module SyntaxSpray
         <html>
         <body>
         <div class="available_games">
-          <% games.each do |name, code| %>
-            <%= name %>
+          <% games.each do |game| %>
+            <%= game.name %>
           <% end %>
         </div>
         </body>
