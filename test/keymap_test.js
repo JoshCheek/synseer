4e8efbe91c620a4392671f6399f1b0934ec18640
Josh Cheek <josh.cheek@gmail.com>
2015-11-05 19:24:51 -0700

    Bring in mocha to test the js

---
+++ test/keymap_test.js
@@ -0,0 +1,8 @@
+// import DefaultKeymap from '../public/js/default_keymap';
+import assert from 'assert'; // https://github.com/joyent/node/blob/9010dd26529cea60b7ee55ddae12688f81a09fcb/lib/assert.js
+
+describe('Parse', ()=>{
+  it('parses single expressions, tracking location information', ()=>{
+    assert.equal('nil', 'nil');
+  });
+});
