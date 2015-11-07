4c8d7d3d48b25881f4f2c2a53d741c9f7cb4e06f
Josh Cheek <josh.cheek@gmail.com>
2015-11-07 05:20:10 -0700

    Unit tests on the keybindings

--- test/keymap_test.js
+++ test/keymap_test.js
@@ -1,8 +1,19 @@
 import * as DefaultKeymap from '../src-js/synseer/default_keymap';
 import assert from 'assert'; // https://github.com/joyent/node/blob/9010dd26529cea60b7ee55ddae12688f81a09fcb/lib/assert.js
 
-describe('Parse', ()=>{
-  it('parses single expressions, tracking location information', ()=>{
-    assert.equal('nil', 'nil');
+describe('DefaultKeymap', ()=>{
+  it('maps each of the keys', ()=>{
+    assert.equal(DefaultKeymap.M,          "send");
+    assert.equal(DefaultKeymap.I,          "int");
+    assert.equal(DefaultKeymap.A,          "array");
+    assert.equal(DefaultKeymap.B,          "begin");
+    assert.equal(DefaultKeymap.F,          "if");
+    assert.equal(DefaultKeymap.L,          "lvar");
+    assert.equal(DefaultKeymap["Shift-L"], "lvasgn");
+    assert.equal(DefaultKeymap["Shift-O"], "op_asgn");
+    assert.equal(DefaultKeymap.S,          "str");
+    assert.equal(DefaultKeymap.W,          "while");
   });
+
+  it('has a keybinding for each type of syntax that we use');
 });
