dcade054bf6fa862fcd74496f0e20127f4be8d4f
Josh Cheek <josh.cheek@gmail.com>
2015-11-08 19:46:53 -0700

    Separate React state and rendering, initialize the component

--- js/synseer/components/stats.js
+++ js/synseer/components/stats.js
@@ -1,11 +1,22 @@
 var React    = require('react');
 var ReactDOM = require('react-dom');
 
+var Stats = React.createClass({
+  getInitialState: function(){
+    return {time: 0, correct: 0, incorrect: 0};
+  },
+  render: function(){
+    return(
+      <div>
+        <div className="time">{this.state.time}</div>
+        <div className="correct">{this.state.correct}</div>
+        <div className="incorrect">{this.state.incorrect}</div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <div>
-    <div className="time"></div>
-    <div className="correct"></div>
-    <div className="incorrect"></div>
-  </div>,
+  <Stats />,
   document.getElementById('stats-react')
 );
